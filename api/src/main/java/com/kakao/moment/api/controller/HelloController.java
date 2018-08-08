02fd4342d0b0c0ecfdc96a49518963df0e8be99f
DESKTOP-ROBIN\teemark <revfactory@gmail.com>
2018-08-09 02:27:14 +0900

    add api module

---
+++ api/src/main/java/com/kakao/moment/api/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.kakao.moment.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public Mono<String> hello() {
+        return Mono.just("hello");
+    }
+}
